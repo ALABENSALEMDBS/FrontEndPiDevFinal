9620a8ac60e2d564ebfa746785f3a39b74c86e71
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-02-24 21:44:48 +0100

    ajouter service tournoi et leur compo

---
+++ src/app/BackOffice/admin-backoffice/tournoi/update-tournoi/update-tournoi.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-update-tournoi',
+  templateUrl: './update-tournoi.component.html',
+  styleUrls: ['./update-tournoi.component.css']
+})
+export class UpdateTournoiComponent {
+
+}
