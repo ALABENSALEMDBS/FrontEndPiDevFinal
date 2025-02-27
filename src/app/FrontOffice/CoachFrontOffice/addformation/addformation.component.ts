5f5efbe2aa387a8b6cde3e21478c11bbfd482949
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-02-27 01:39:42 +0100

    card

--- src/app/FrontOffice/CoachFrontOffice/addformation/addformation.component.ts
+++ src/app/FrontOffice/CoachFrontOffice/addformation/addformation.component.ts
@@ -10,8 +10,13 @@ import { FormationService } from 'src/app/services/serviceCoatch/serviceformatio
     standalone: false
 })
 export class AddformationComponent {
-  formationOptions = ["3-3-4", "5-2-3", "4-4-2"]; // Liste des formats disponibles
-
+  formationOptions = [
+    "3-3-4", "5-2-3", "4-4-2", // vos formations de base
+    "4-3-3", "4-5-1", "3-5-2", "3-4-3", 
+    "4-2-3-1", "5-3-2", "5-4-1", "4-1-4-1", 
+    "4-4-1-1", "4-3-2-1", "3-6-1"
+  ];
+  
   formationForm: FormGroup;
     successMessage: string = '';
     errorMessage: string = '';
