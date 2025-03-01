d429266ab14d576e14d13cbfba09712635bfb9ae
ALABENSALEMDBS <bensalem.ala@esprit.tn>
2025-03-01 18:04:03 +0100

    list joueurs

--- src/core/models/formation.ts
+++ src/core/models/formation.ts
@@ -1,5 +1,11 @@
+import { Joueurs } from "./joueur";
+import { tactic } from "./tactic";
+
 export class formation{
     idFormation!: number;
     nameFormation!: string;
     descriptionFormation!: string;
+
+    tactics?: tactic[];
+    joueurs?: Joueurs[];
     }
