6e05a68132f64d0a78fac4241fd0be69a0c7cc9a
MaherKouki <koukimaher386@gmail.com>
2025-05-03 01:38:03 +0100

    cup for visitor

---
+++ src/app/FrontOffice/HomePages/competition-display/competition-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-display',
+  imports: [],
+  templateUrl: './competition-display.component.html',
+  styleUrl: './competition-display.component.css'
+})
+export class CompetitionDisplayComponent {
+
+}
---
+++ src/app/FrontOffice/HomePages/competition-standings-display/competition-standings-display.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-competition-standings-display',
+  imports: [],
+  templateUrl: './competition-standings-display.component.html',
+  styleUrl: './competition-standings-display.component.css'
+})
+export class CompetitionStandingsDisplayComponent {
+
+}
